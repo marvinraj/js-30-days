0dc3b3bf39009f90f85572f1d20d8339bf83c860
Marvin <marvinraj77@gmail.com>
2024-01-29 15:33:13 +0800

    completed day 1

---
+++ day-1/intro.js
@@ -0,0 +1,12 @@
+let firstName = "marvin"
+let lastName = "raj"
+let age = 23
+let weight = 70
+
+console.log("first name = ", firstName)
+console.log("last name = ", lastName)
+console.log("age = ", age)
+console.log("weight = ", weight)
+
+// this is a comment
+// this is also a comment 
