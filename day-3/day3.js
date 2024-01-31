cc35edba9f23bb7c3f7cf418a41c523e189b8a72
Marvin <marvinraj77@gmail.com>
2024-01-31 11:03:02 +0800

    completed day 3

--- day-3/day3.js
+++ day-3/day3.js
@@ -1,2 +1,53 @@
 // booleans!!!!!!
 
+// 1 - truthy vs falsy
+
+
+// 2 - operators
+// 2.1 - assignment operators
+// --- +,-,=, +=, -=, *=, /=
+
+// 2.2 - arithmetic operator
+// -- +, -, *, /, %, **
+
+// 2.3 - comparison operators
+// ==, ===, !=, >, <, >=, <=
+
+// 2.4 - logical operators
+// ++num, num++, --num, num--
+
+// 2.5 - ternary operators
+// look thru this later
+
+// 2.6 - operator precedence
+// look thru this later
+
+
+// 3 - window method
+// alert(), prompt(), confirm()
+
+
+// 4 - date object
+const now = new Date()
+console.log(now)
+
+yearNow = now.getFullYear()
+console.log(yearNow)
+
+monthNow = now.getMonth()
+console.log(monthNow)
+
+dateNow = now.getDate()
+console.log(dateNow)
+
+dayNow = now.getDay()
+console.log(dayNow)
+
+hourNow = now.getHours()
+console.log(hourNow)
+
+minuteNow = now.getMinutes()
+console.log(minuteNow)
+
+timeNow = now.getTime()
+console.log(timeNow)
